483ff2d93207278590cae45cfbfea44cc60e3afb
tony <antbradley91@gmail.com>
2021-05-13 07:13:57 -0400

    move to pnpm and increase test coverage

--- tests/CalendarTest.elm
+++ tests/CalendarTest.elm
@@ -12,8 +12,8 @@ july14th2020 =
     millisToPosix 1594725024442
 
 
-july14th2020calendar =
-    fromTime Nothing utc july14th2020
+july14th2020calendar config =
+    fromTime config utc july14th2020
 
 
 suite : Test
@@ -21,7 +21,7 @@ suite =
     describe "initializes calendar"
         [ test "First day is sunday" <|
             \_ ->
-                july14th2020calendar
+                july14th2020calendar Nothing
                     |> List.head
                     |> Maybe.andThen List.head
                     |> Maybe.map .weekdayNumber
@@ -29,7 +29,7 @@ suite =
                     |> Maybe.withDefault (Expect.fail "Did not generate calendar")
         , test "July 1st is wednesday" <|
             \_ ->
-                july14th2020calendar
+                july14th2020calendar Nothing
                     |> List.head
                     |> Maybe.map Array.fromList
                     |> Maybe.andThen (Array.get 3)
@@ -39,15 +39,19 @@ suite =
                     |> Maybe.withDefault (Expect.fail "Did not generate calendar")
         , test "Print functionality" <|
             \_ ->
-                july14th2020calendar
+                july14th2020calendar (Just { startWeekday = Wed })
                     |> print
                     |> (\_ -> Expect.pass)
         , test "Can initialize from time" <|
             \_ ->
+                let
+                    config =
+                        Just { startWeekday = Tue }
+                in
                 Date.fromCalendarDate 2020 Jul 14
-                    |> fromDate Nothing
+                    |> fromDate config
                     |> print
-                    |> Expect.equal (print july14th2020calendar)
+                    |> Expect.equal (print (july14th2020calendar config))
         , test "Correctly populates the correct days" <|
             \_ ->
                 let
